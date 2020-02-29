f0df539b19ad19a1fcc8ae70b355563287ca7cb1
Loic Fremont <lc.fremont@gmail.com>
2020-02-29 01:20:08 +0100

    Move admin recipe src into AdminBundle

---
+++ Recipe/spec/App/Dashboard/DashboardStatisticsProviderSpec.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace spec\App\Dashboard;
+
+use App\Dashboard\DashboardStatisticsProvider;
+use App\Dashboard\Statistics\StatisticInterface;
+use PhpSpec\ObjectBehavior;
+
+class DashboardStatisticsProviderSpec extends ObjectBehavior
+{
+    function let(StatisticInterface $firstStatistic, StatisticInterface $secondStatistic): void
+    {
+        $this->beConstructedWith([$firstStatistic, $secondStatistic]);
+    }
+
+    function it_is_initializable()
+    {
+        $this->shouldHaveType(DashboardStatisticsProvider::class);
+    }
+
+    function it_generate_statistics_for_each_providers(
+        StatisticInterface $firstStatistic, StatisticInterface $secondStatistic
+    ): void {
+        $firstStatistic->generate()->shouldBeCalled();
+        $secondStatistic->generate()->shouldBeCalled();
+
+        $this->getStatistics();
+    }
+
+    function it_throws_an_invalid_argument_exception_when_statistic_provider_does_not_implements_the_interface(
+        \stdClass $statistic
+    ): void {
+        $this->beConstructedWith([$statistic]);
+
+        $this->shouldThrow(\InvalidArgumentException::class)->during('getStatistics');
+    }
+}
---
+++ Recipe/src/Dashboard/DashboardStatisticsProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Dashboard;
+
+use App\Dashboard\Statistics\StatisticInterface;
+use Webmozart\Assert\Assert;
+
+class DashboardStatisticsProvider
+{
+    /** @var iterable&StatisticInterface[] */
+    private $statistics;
+
+    public function __construct(iterable $statistics)
+    {
+        $this->statistics = $statistics;
+    }
+
+    public function getStatistics(): array
+    {
+        $statistics = [];
+        foreach ($this->statistics as $statistic) {
+            Assert::implementsInterface($statistic, StatisticInterface::class, sprintf('Class %s must implement %s', get_class($statistic), StatisticInterface::class));
+            $statistics[] = $statistic->generate();
+        }
+
+        return $statistics;
+    }
+}
